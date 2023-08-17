fb4159d1ebfb39149bbac6d90c3fd575bec47a64
lhebia <lhebia@gmail.com>
2023-08-17 12:05:52 -0400

    add bike route and controller

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,50 @@
+class BikesController < ApplicationController
+    before_action :set_bike, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bikes = Bike.all
+  end
+
+  def show
+  end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+
+    if @bike.save
+      redirect_to bikes_path, notice: "Bike was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @bike.update(bike_params)
+      redirect_to bikes_path, notice: "Bike was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bike.destroy
+    redirect_to bikes_path, notice: "Bike was successfully destroyed."
+  end
+
+  private
+
+  def set_bike
+    @bike = Bike.find(params[:id])
+  end
+
+  def bike_params
+    params.require(:bike).permit(:name)
+  end
+end

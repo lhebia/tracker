e394a115ad2ab795938e7be3f3254b234314fc9d
lhebia <lhebia@gmail.com>
2023-11-01 16:56:13 -0400

    broadcast updates to model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,4 +2,10 @@ class Bike < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # The below three lines of code are equivalent to the last line
+  # after_create_commit -> { broadcast_prepend_later_to "bikes" }
+  # after_update_commit -> { broadcast_replace_later_to "bikes" }
+  # after_destroy_commit -> { broadcast_remove_to "bikes" }
+  broadcasts_to ->(bike) { "bikes" }, inserts_by: :prepend
 end

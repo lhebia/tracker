25295a1df8a4a2771e0bdfd3871943587da72742
lhebia <lhebia@gmail.com>
2023-11-01 15:37:37 -0400

    turbofy edit and delete

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -35,7 +35,11 @@ class BikesController < ApplicationController
 
   def destroy
     @bike.destroy
-    redirect_to bikes_path, notice: "Bike was successfully destroyed."
+
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream
+    end
   end
 
   private

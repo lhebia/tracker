9e75002341e6cb7f48a6693b4f3ff7a860fff74a
lhebia <lhebia@gmail.com>
2023-11-02 16:17:39 -0400

    update seeds for rides

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,20 @@ user2 = User.create(email: "user2@lhebia.com", password: "password", username: "
 
 puts "\n== Seeding users complete 👨🏻‍💻 =="
 
-Bike.create(name: "Neo", make: "Specialized", model: "Tarmac SL7", description: "One bike to rule them all", serial_number: 1, user_id: user.id, year: 2023)
+bike = Bike.create(name: "Neo", make: "Specialized", model: "Tarmac SL7", description: "One bike to rule them all", serial_number: 1, user_id: user.id, year: 2023)
 Bike.create(name: "Caaderific", make: "Cannondale", model: "CAAD10", description: "CAAD have them all", serial_number: 2, user_id: user.id, year: 2014)
 Bike.create(name: "Dream bike", make: "Cervelo", model: "Soloist", description: "Do it all race bike", serial_number: 3, user_id: user.id, year: 2024)
 
-Bike.create(name: "Standert", make: "Standert", model: "Kreissage", description: "Do it all race bike", serial_number: 3, user_id: user2.id, year: 2024)
+bike2 = Bike.create(name: "Standert", make: "Standert", model: "Kreissage", description: "Do it all race bike", serial_number: 3, user_id: user2.id, year: 2024)
 
 puts "\n== Seeding bikes complete 🚲 =="
+
+Ride.create(name: "The longest ever", date: Date.current, start_time: DateTime.now - 2.hours, end_time: DateTime.now, distance: 160, elevation: 250, description: "It was hella long", user_id: user.id, bike_id: bike.id)
+Ride.create(name: "A long ride", date: Date.current - 2.days, start_time: DateTime.now - 48.hours, end_time: DateTime.now - 46.hours, distance: 100, elevation: 200, description: "It was long", user_id: user.id, bike_id: bike.id)
+Ride.create(name: "Wow, that was long", date: Date.current - 3.days, start_time: DateTime.now - 72.hours, end_time: DateTime.now - 71.hours, distance: 80, elevation: 150, description: "It was quite long", user_id: user.id, bike_id: bike.id)
+
+Ride.create(name: "My longest ever", date: Date.current, start_time: DateTime.now - 1.hour, end_time: DateTime.now, distance: 100, elevation: 200, description: "It was long", user_id: user2.id, bike_id: bike2.id)
+
+puts "\n== Seeding rides complete 🎢 =="
 
 puts "\n== Seeding is all done 🪴 =="

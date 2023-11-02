08504d1a1a86d3b6b66f46a6cff4be35638f50fa
lhebia <lhebia@gmail.com>
2023-11-02 12:03:38 -0400

    update streams, navbar and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,18 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts "\n== Seeding the database with fixtures 🌱 =="
-system("bin/rails db:fixtures:load")
+
+user = User.create(email: "user@lhebia.com", password: "password", username: "User", first_name: "User", last_name: "Dude")
+user2 = User.create(email: "user2@lhebia.com", password: "password", username: "User2", first_name: "User2", last_name: "Dude")
+
+puts "\n== Seeding users complete 👨🏻‍💻 =="
+
+Bike.create(name: "Neo", make: "Specialized", model: "Tarmac SL7", description: "One bike to rule them all", serial_number: 1, user_id: user.id, year: 2023)
+Bike.create(name: "Caaderific", make: "Cannondale", model: "CAAD10", description: "CAAD have them all", serial_number: 2, user_id: user.id, year: 2014)
+Bike.create(name: "Dream bike", make: "Cervelo", model: "Soloist", description: "Do it all race bike", serial_number: 3, user_id: user.id, year: 2024)
+
+Bike.create(name: "Standert", make: "Standert", model: "Kreissage", description: "Do it all race bike", serial_number: 3, user_id: user2.id, year: 2024)
+
+puts "\n== Seeding bikes complete 🚲 =="
 
 puts "\n== Seeding is all done 🪴 =="

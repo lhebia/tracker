b89e96c31ca9dc6f53f65095c9b97cd6ae792879
lhebia <lhebia@gmail.com>
2024-10-29 14:16:36 -0400

    Update name columns on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
   has_many :bikes, dependent: :destroy
   has_many :rides, dependent: :destroy
 
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+
   def full_name
     first_name.capitalize + " " + last_name.capitalize
   end

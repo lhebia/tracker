f6a29dbdd73048641fa38b502111f06e63e4d1da
lhebia <lhebia@gmail.com>
2024-11-01 15:32:21 -0400

    Fix duration method

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -9,7 +9,7 @@ class Ride < ApplicationRecord
   scope :for_user, -> (user) { where(user_id: user.id) }
 
   def duration_in_mins
-    return 0 if end_time.nil? || stat_time.nil?
+    return 0 if end_time.nil? || start_time.nil?
     ((end_time - start_time) / 60).to_i
   end
 end
